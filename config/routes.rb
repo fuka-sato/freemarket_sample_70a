9dbe8e304fa20316c9a3b95079e5e816b8c10b61
acky-k <info@aki-no-shirabe.com>
2020-05-18 13:26:02 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  resources :items do
+    collection do
+      get 'confirm'
+    end
+  end
+  
   resources :users, only: :index do
     collection do
       get :onestep
