d6c4f2eb68a417fa12bd455a2cbd50fa851ad256
fuyuhaonabe <sls_asa_1206@yahoo.co.jp>
2020-05-24 17:31:20 +0900

    haml等修正

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,33 @@
 class Profile < ApplicationRecord
 
+  validates :family_name,
+  presence: true,                     # 必須にしたい！
+  format: { with: /\A[ぁ-んァ-ン一-龥]/ } 
+
+  validates :first_name,
+  presence: true,
+  format: { with: /\A[ぁ-んァ-ン一-龥]/ } 
+
+  validates :family_name_kana,
+  presence: true,
+  format: { with: /\A[ァ-ヶー－]+\z/ } 
+
+  validates :first_name_kana,
+  presence: true,
+  format: { with: /\A[ァ-ヶー－]+\z/ } 
+
+  validates :birth_year,
+  presence: true,
+  format: { with: /\A[0-9]+\z/ } 
+
+  validates :birth_month,
+  presence: true,
+  format: { with: /\A[0-9]+\z/ } 
+
+  validates :birth_day,
+  presence: true,
+  format: { with: /\A[0-9]+\z/ } 
+
   belongs_to :user
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, uniqueness: true
+  validates :nickname,
+  presence: true,                     # 必須にしたい！
+  uniqueness: true,                   # 一意性
+  length: { maximum: 20 }            # あんまり長いのも……
+
+  validates :email,
+  presence: true,
+  uniqueness: true,
+  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }  # メールアドレスの正規表現
+
+  validates :encrypted_password,
+  presence: true,                     # 必須にしたい！
+  length: { minimum: 7 },            # 7文字以上
+  format: { with: /\A[a-z0-9]+\z/i }  # やっぱり半角英数字のみだよね！
 
   has_one :profile
   accepts_nested_attributes_for :profile
