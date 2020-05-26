acd18c2fbf93a16fac65799c547262b0fb99e5d2
acky-k <info@aki-no-shirabe.com>
2020-05-26 17:15:33 +0900

    ancestry実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,7 +14,20 @@ class ItemsController < ApplicationController
   end
 
   def new
-    # @item = Item.new
+    # @item = Item.new 
+    @category_parent_array = ["---"]
+    
+    Category.where(ancestry: nil).each do |parent|  
+    @category_parent_array << parent.name  
+    end
+  end
+
+  def get_category_children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children　// 子カテゴリの配列（name）
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children　　// 孫カテゴリの配列（name）
   end
 
   def confirm
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
   def onestep
-    
   end
 end
