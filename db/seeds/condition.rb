84689337e4fc5f7e2d7b4e9521514b257a73b79b
fuyuhaonabe <sls_asa_1206@yahoo.co.jp>
2020-06-07 15:57:24 +0900

    seedファイル準備と整理

---
+++ db/seeds/condition.rb
@@ -0,0 +1,8 @@
+conditions = Condition.create([
+  {condition: "新品・未使用"},
+  {condition: "未使用に近い"},
+  {condition: "目立った傷や汚れなし"},
+  {condition: "やや傷や汚れあり"},
+  {condition: "傷や汚れあり"},
+  {condition: "全体的に状態が悪い"}
+                              ])
---
+++ db/seeds/delivery_price.rb
@@ -0,0 +1,4 @@
+delivery_prices = DeliveryPrice.create([
+  {delivery_price: "送料込み（出品者負担）"},
+  {delivery_price: "着払い（購入者負担）"}
+  ])
