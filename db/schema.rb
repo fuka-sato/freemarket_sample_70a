10a8b8ca66cd1b754992b842346c180141155d11
fuyuhaonabe <sls_asa_1206@yahoo.co.jp>
2020-06-07 21:10:00 +0900

    migrationファイル調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_064325) do
+ActiveRecord::Schema.define(version: 2020_06_07_065059) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "brand", null: false
