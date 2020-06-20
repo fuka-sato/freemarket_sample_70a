da56c34075474cd7f26c9108e4899f6ba8f29ae1
fuka-sato <61341002+fuka-sato@users.noreply.github.com>
2020-06-20 17:37:25 +0900

    不要なコメントアウトの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,12 +20,6 @@ class ItemsController < ApplicationController
       @item.item_images.build
       #セレクトボックスの初期値設定
       @category_parent_array = ["---"]
-
-      
-      #データベースから、親カテゴリーのみ抽出し、配列化
-      # Category.where(ancestry: nil).each do |parent|
-      # @category_parent_array << parent
-      # end
     else
       redirect_to root_path
     end
@@ -40,20 +34,13 @@ class ItemsController < ApplicationController
     end 
   end
 
-  def destroy
-    @item.destroy
-    redirect_to root_path
-  end
 
   def get_category_children
-  #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-  # @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
   @category_children = Category.find(params[:parent_name]).children
   end
   # 子カテゴリーが選択された後に動くアクション
   def get_category_grandchildren
   #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
-  # @category_grandchildren = Category.find("#{params[:child_id]}").children
   @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
 
