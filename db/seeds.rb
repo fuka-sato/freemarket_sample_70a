962e7f95c8883dac05fb4dfea9d7b3dcd95e1dd5
dinnerdaichi <yamaguchi.daichandayo@gmail.com>
2020-06-28 12:53:09 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # require ‘./db/seeds/category.rb’
-require ‘./db/seeds/brand.rb’
-require ‘./db/seeds/condition.rb’
-require ‘./db/seeds/delivery_area.rb’
-require ‘./db/seeds/delivery_day.rb’
-require ‘./db/seeds/delivery_price.rb’
-require ‘./db/seeds/size.rb’
+require "./db/seeds/brand.rb"
+require "./db/seeds/condition.rb"
+require "./db/seeds/delivery_area.rb"
+require "./db/seeds/delivery_day.rb"
+require "./db/seeds/delivery_price.rb"
+require "./db/seeds/size.rb"
