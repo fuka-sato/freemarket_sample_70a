708ca16851d54024db86d064ef534ae2b8975f9d
yukarin-i <paricollection1983@gmail.com>
2020-07-11 17:27:27 +0900

    コメントアウトの削除とリファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -98,20 +98,13 @@ class ItemsController < ApplicationController
     child_category = grandchild_category.parent
 
     @category_parent_array = []
-    Category.where(ancestry: nil).each do |parent|
-      @category_parent_array << parent.name
-    end
-
+    Category.pluck(:ancestry) 
+    
     @category_children_array = []
-    Category.where(ancestry: child_category.ancestry).each do |children|
-      @category_children_array << children
-    end
+    Category.pluck(:ancestry) 
 
     @category_grandchildren_array = []
-    Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
-      @category_grandchildren_array << grandchildren
-    end
-
+    Category.pluck(:ancestry)
   end
 
   
